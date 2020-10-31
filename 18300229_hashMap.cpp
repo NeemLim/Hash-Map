d1290bb2614f98a0364978137370af0727354782
NeemLim <luismlimp@outlook.com>
2020-10-31 02:55:54 -0600

    added hash Class with 2 methods.

--- 18300229_hashMap.cpp
+++ 18300229_hashMap.cpp
@@ -148,29 +148,44 @@ public:
 		}
 		return false;
 	}
+};
 
-	int encode(string uncodedStr)	//casts each character into int and returns the sum.
+template <class T>
+class cHash
+{
+	int encode(T uncodedStr)	//casts each character into int and returns the sum.
 	{
 		int code = 0;
 		for (int i = 0; i < uncodedStr.size(); i++)
 			code += (int)uncodedStr[i];
 		return code;
 	}
-
-	int getBucketPos(string uncodedStr, int bucketNum)	//returns
+public:
+	int getBucketPos(T uncodedStr, int bucketNum)	//returns
 	{
 		int bucketPos = encode(uncodedStr) % bucketNum;
 		return bucketPos;
+	}
+	int allCount(list<T> linkedList[], int bucketNum)
+	{
+		int countSum = 0;
+		for (int i = 0; i < bucketNum; i++)
+		{
+			countSum += linkedList[bucketNum].count();
+		}
+
+		return countSum;
 	}
 };
 
 int main()
 {
-	list<string> hashMap;
-	string dataValue;
+	cHash<string> hashMap;
+	list<string> bucket[10];
+	string itemValue;
 	int searchPos = 0,
-		bucketNum = 0,
-		bucketPos = 10;
+		bucketNum = 10,
+		bucketPos = 0;
 	do
 	{
 		char choice = 0;
@@ -204,73 +219,79 @@ int main()
 			{
 				if ((GetAsyncKeyState(VK_LCONTROL)) == false)
 				{
-					cout << "Element [" << hashMap.count() + 1 << "]: ";
-					getline(cin, dataValue);
-					cout << endl << hashMap.getBucketPos(dataValue, bucketPos);
-					hashMap.addItem(dataValue);
+					cout << "Add element: ";
+					getline(cin, itemValue);
+					bucketPos = hashMap.getBucketPos(itemValue, bucketNum);
+					bucket[bucketPos].addItem(itemValue);
+					cout << bucket[bucketPos].getValue(1);
 				}
 				GetAsyncKeyState; //Gets the current pressed key from the user.
 			}
 			break;
 
-		case '2':	//Search
-			if (hashMap.checkEmpty())
-				break;
-			cout << "Input the value of the element you wish to search: "; cin >> dataValue;
-			(hashMap.getPosition(dataValue) == -1) ?
-				cout << "\nNo matches have been found.\n"
-				:
-				cout << "\n>Matching value found \n";
+		case '2':	//Count
+			itemValue = hashMap.allCount(bucket, bucketNum);
+			cout << "The total count of elements = " << itemValue << endl;
 			break;
 
-		case '3': //Show Value
-			if (hashMap.checkEmpty())
-				break;
-			cout << "Input the position of the element you wish to search: "; cin >> searchPos;
-			(searchPos > hashMap.count() or searchPos <= 0) ?
-				cout << "\n>Out of boundaries.\n"
-				:
-				cout << "\n>Matching value in that postion = " << hashMap.getValue(searchPos - 1) << endl;
-			break;
+			//case '2':	//Search
+			//	if (bucket.checkEmpty())
+			//		break;
+			//	cout << "Input the value of the element you wish to search: "; cin >> itemValue;
+			//	(bucket.getPosition(itemValue) == -1) ?
+			//		cout << "\nNo matches have been found.\n"
+			//		:
+			//		cout << "\n>Matching value found \n";
+			//	break;
 
-		case '4': //Count
-			if (hashMap.checkEmpty())
-				break;
-			cout << "\nThe element count is = " << hashMap.count() << " elements.\n";
-			break;
+			//case '3': //Show Value
+			//	if (bucket.checkEmpty())
+			//		break;
+			//	cout << "Input the position of the element you wish to search: "; cin >> searchPos;
+			//	(searchPos > bucket.count() or searchPos <= 0) ?
+			//		cout << "\n>Out of boundaries.\n"
+			//		:
+			//		cout << "\n>Matching value in that postion = " << bucket.getValue(searchPos - 1) << endl;
+			//	break;
 
-		case '5': //Show all items
-			if (hashMap.checkEmpty())
-				break;
-			for (int i = 0; i < hashMap.count(); i++)
-				cout << "Item [" << i + 1 << "] = " << hashMap.getValue(i) << endl;
-			break;
+			//case '4': //Count
+			//	if (bucket.checkEmpty())
+			//		break;
+			//	cout << "\nThe element count is = " << bucket.count() << " elements.\n";
+			//	break;
 
-		case '6': //Update
-			if (hashMap.checkEmpty())
-				break;
-			cout << "Input the value of the element you wish to encode: ";
-			getline(cin, dataValue);
-			cout << hashMap.encode(dataValue);
-			break;
+			//case '5': //Show all items
+			//	if (bucket.checkEmpty())
+			//		break;
+			//	for (int i = 0; i < bucket.count(); i++)
+			//		cout << "Item [" << i + 1 << "] = " << bucket.getValue(i) << endl;
+			//	break;
 
-		case '7':	//Delete particular
-			if (hashMap.checkEmpty()) break;
-			cout << "Input the value of the element you wish to delete: ";  cin >> dataValue;
-			hashMap.deleteParticular(dataValue);
-			break;
+			//case '6': //Update
+			//	if (bucket.checkEmpty())
+			//		break;
+			//	cout << "Input the value of the element you wish to encode: ";
+			//	getline(cin, itemValue);
+			//	cout << bucket.encode(itemValue);
+			//	break;
 
-		case '8':	//Clear list
-			if (hashMap.checkEmpty())
-				break;
-			hashMap.deleteAll();
-			cout << "\n hashMap is now clear, all elements deleted successfully\n";
-			break;
+			//case '7':	//Delete particular
+			//	if (bucket.checkEmpty()) break;
+			//	cout << "Input the value of the element you wish to delete: ";  cin >> itemValue;
+			//	bucket.deleteParticular(itemValue);
+			//	break;
 
-		case '9':	//Exit
-			cout << endl << "Successfully exited the program.\n";
-			return 0;
-			break;
+			//case '8':	//Clear list
+			//	if (bucket.checkEmpty())
+			//		break;
+			//	bucket.deleteAll();
+			//	cout << "\n hashMap is now clear, all elements deleted successfully\n";
+			//	break;
+
+			//case '9':	//Exit
+			//	cout << endl << "Successfully exited the program.\n";
+			//	return 0;
+			//	break;
 		}
 		cout << endl;
 		system("pause");
